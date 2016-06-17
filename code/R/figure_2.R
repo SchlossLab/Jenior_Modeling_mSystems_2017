51457bf552de6db3cff5f9433462971e318712ce
Matt Jenior <mljenior@umich.edu>
2016-06-17 19:00:11 -0400

    working on figure 2 pathway bat plots

--- code/R/figure_2.R
+++ code/R/figure_2.R
@@ -1,158 +1,155 @@
 
+deps <- c('wesanderson');
+for (dep in deps){
+  if (dep %in% installed.packages()[,"Package"] == FALSE){
+    install.packages(as.character(dep), quiet=TRUE);
+  }
+  library(dep, verbose=FALSE, character.only=TRUE)
+}
+rm(dep, deps)
+
+#--------------------------------------------------------------------------------------------------------------#
+
 # Define variables
 cefoperazone_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/data/mapping/cdifficile630/cefoperazone_630.RNA_reads2cdf630.norm.annotated.txt'
 clindamycin_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/data/mapping/cdifficile630/clindamycin_630.RNA_reads2cdf630.norm.annotated.txt'
 streptomycin_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/data/mapping/cdifficile630/streptomycin_630.RNA_reads2cdf630.norm.annotated.txt'
 germfree_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/data/mapping/cdifficile630/germfree.RNA_reads2cdf630.norm.annotated.txt'
 
+# Open files
 cefoperazone <- read.delim(cefoperazone_file, sep='\t', header=FALSE, row.names=1)
 colnames(cefoperazone) <- c('Cefoperazone', 'ko', 'gene', 'pathway')
 clindamycin <- read.delim(clindamycin_file, sep='\t', header=FALSE, row.names=1)
 colnames(clindamycin) <- c('Clindamycin', 'ko', 'gene', 'pathway')
 streptomycin <- read.delim(streptomycin_file, sep='\t', header=FALSE, row.names=1)
 colnames(streptomycin) <- c('Streptomycin', 'ko', 'gene', 'pathway')
+germfree <- read.delim(germfree_file, sep='\t', header=FALSE, row.names=1)
+colnames(germfree) <- c('Germfree', 'ko', 'gene', 'pathway')
+
+# Clean up
+rm(cefoperazone_file, clindamycin_file, streptomycin_file, germfree_file)
+
+#--------------------------------------------------------------------------------------------------------------#
+
+# Format data for merging
+cefoperazone$ko <- NULL
+cefoperazone$gene <- NULL
+cefoperazone$pathway <- NULL
+clindamycin$ko <- NULL
+clindamycin$gene <- NULL
+clindamycin$pathway <- NULL
+streptomycin$ko <- NULL
+streptomycin$gene <- NULL
+streptomycin$pathway <- NULL
+
+# Merge tables
+combined_mapping <- merge(cefoperazone, clindamycin, by='row.names')
+rownames(combined_mapping) <- combined_mapping$Row.names
+combined_mapping$Row.names <- NULL
+combined_mapping <- merge(combined_mapping, streptomycin, by='row.names')
+rownames(combined_mapping) <- combined_mapping$Row.names
+combined_mapping$Row.names <- NULL
+combined_mapping <- merge(combined_mapping, germfree, by='row.names')
+rownames(combined_mapping) <- combined_mapping$Row.names
+combined_mapping$Row.names <- NULL
+
+# Rarefy mappings to be equal within sequencing type
+sub_size <- round(min(colSums(combined_mapping[,1:4])) * 0.9) # 34050
+combined_mapping$Cefoperazone <- t(rrarefy(combined_mapping$Cefoperazone, sample=sub_size))
+combined_mapping$Clindamycin <- t(rrarefy(combined_mapping$Clindamycin, sample=sub_size))
+combined_mapping$Streptomycin <- t(rrarefy(combined_mapping$Streptomycin, sample=sub_size))
+combined_mapping$Germfree <- t(rrarefy(combined_mapping$Germfree, sample=sub_size))
+
+# Eliminate genes with no transcripts mapping
+combined_mapping <- combined_mapping[rowSums(combined_mapping[,1:4]) != 0, ] 
+
+# Clean up
+rm(cefoperazone, clindamycin, streptomycin, germfree, sub_size)
+
+# Remove unused catagories
+combined_mapping$ko <- NULL
+combined_mapping$gene <- NULL
+
+#--------------------------------------------------------------------------------------------------------------#
+
+# Subset by KEGG catagory and pool
+
+# Metabolism - not working
+carbohydrate <- subset(combined_mapping, grepl('Carbohydrate_metabolism', combined_mapping$pathway))
+carbohydrate <- colSums(carbohydrate[,1:4])
+energy <- subset(combined_mapping, grepl('Energy_metabolism', combined_mapping$pathway))
+energy <- colSums(energy[,1:4])
+lipid <- subset(combined_mapping, grepl('Lipid_metabolism', combined_mapping$pathway))
+lipid <- colSums(lipid[,1:4])
+nucleotide <- subset(combined_mapping, grepl('Nucleotide_metabolism', combined_mapping$pathway))
+nucleotide <- colSums(nucleotide[,1:4])
+amino_acid <- subset(combined_mapping, grepl('Amino_acid_metabolism', combined_mapping$pathway))
+amino_acid <- colSums(amino_acid[,1:4])
+other_amino_acids <- subset(combined_mapping, grepl('Metabolism_of_other_amino_acids', combined_mapping$pathway))
+other_amino_acids <- colSums(other_amino_acids[,1:4])
+glycan <- subset(combined_mapping, grepl('Glycan_biosynthesis_and_metabolism', combined_mapping$pathway))
+glycan <- colSums(glycan[,1:4])
+cofactors_and_vitamins <- subset(combined_mapping, grepl('Metabolism_of_cofactors_and_vitamins', combined_mapping$pathway))
+cofactors_and_vitamins <- colSums(cofactors_and_vitamins[,1:4])
+terpenoids_and_polyketides <- subset(combined_mapping, grepl('Metabolism_of_terpenoids_and_polyketides', combined_mapping$pathway))
+terpenoids_and_polyketides <- colSums(terpenoids_and_polyketides[,1:4])
+secondary_metabolites <- subset(combined_mapping, grepl('Biosynthesis_of_other_secondary_metabolites', combined_mapping$pathway))
+secondary_metabolites <- colSums(secondary_metabolites[,1:4])
+xenobiotics <- subset(combined_mapping, grepl('Xenobiotics_biodegradation_and_metabolism', combined_mapping$pathway))
+xenobiotics <- colSums(xenobiotics[,1:4])
+genetics <- subset(combined_mapping, grepl('Genetic_Information_Processing', combined_mapping$pathway))
+genetics <- colSums(genetics[,1:4])
+motility <- subset(combined_mapping, grepl('Cell_motility', combined_mapping$pathway))
+motility <- colSums(motility[,1:4])
+rm(combined_mapping)
+
+# Assemble final table
+pooled_mapping <- rbind(carbohydrate, energy, lipid, nucleotide, amino_acid, other_amino_acids, glycan,
+                        cofactors_and_vitamins, terpenoids_and_polyketides, secondary_metabolites,
+                        xenobiotics, genetics, motility)
+
+#--------------------------------------------------------------------------------------------------------------#
+
+# Pooling pathways
+cefoperazone_pooled_pathways <- aggregate(transcripts ~ pathway_name, FUN = sum, data = cefoperazone_pathways)  
+rownames(cefoperazone_pooled_pathways) <- cefoperazone_pooled_pathways[,1]
+cefoperazone_pooled_pathways[,1] <- NULL
+colnames(cefoperazone_pooled_pathways) <- 'Cefoperazone'
+
+
+
 
 
 
 #--------------------------------------------------------------------------------------------------------------#
 
-# Define formatting functions
-format_expression_aggregate <- function(file_name){
+fox <- wes_palette("FantasticFox")
+plot_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/results/figures/figure_2.pdf'
 
-  # Load in and format data
-  expression <- read.delim(file_name, sep='\t', header=F)
-  colnames(expression) <- c('Normalized_abundance', 'Gene_code', 'Gene_name', 'KO', 'Pathway_code', 'Pathway_name', 'Pathway_family1', 'Pathway_family2')
-  path_expression <- subset(expression, Pathway_code != 'path_key_error')
-  #path_expression <- subset(expression, Pathway_code != 'pathway_unannotated')
-  path_expression$Relabund <- (path_expression$Normalized_abundance  / sum(path_expression$Normalized_abundance)) * 100
+# Open a PDF
+pdf(file=plot_file, width=12, height=7)
 
-  # Pool data in same pathway category and sort
-  pool_path_expression <- aggregate(Relabund ~ Pathway_name, FUN = sum, data = path_expression)
-  pool_path_expression <- pool_path_expression[order(-pool_path_expression$Relabund), ] 
-  rownames(pool_path_expression) <- pool_path_expression$Pathway_name
-  pool_path_expression$Pathway_name <- NULL
-  rows <- rownames(pool_path_expression)
-  
-  # Bin low abundant catagories into 'Other'
-  other <- sum(pool_path_expression$Relabund[which(pool_path_expression$Relabund < 0.3)])
-  pool_path_expression[pool_path_expression < 0.3] <- 0
-  pool_path_expression <- rbind(pool_path_expression, other)
-  rows <- append(rows, 'Other')
-  rownames(pool_path_expression) <- rows
-  
-  return(pool_path_expression)
+# Create layout for multi-plot
+layout(mat=matrix(c(1,1, 
+                    2,2), 
+                  nrow=2, ncol=2, byrow=TRUE))
 
-}
+# Lowly abundant
+par(mar=c(3,2,1,1))
+barchart()
 
 
-format_expression<- function(file_name){
-  expression <- read.delim(file_name, sep='\t', header=F)
-  colnames(expression) <- c('Normalized_abundance', 'Gene_code', 'Gene_name', 'KO', 'Pathway_code', 'Pathway_name', 'Pathway_family1', 'Pathway_family2')
-  path_expression <- subset(expression, Pathway_code != 'path_key_error')
-  #path_expression <- subset(expression, Pathway_code != 'pathway_unannotated')
-  path_expression$Relabund <- (path_expression$Normalized_abundance  / sum(path_expression$Normalized_abundance)) * 100
-  rownames(path_expression) <- path_expression$Gene_code
-  #path_expression$Gene_code <- NULL
-  path_expression <- path_expression[order(-path_expression$Normalized_abundance), ]
-  #path_expression$Gene_name <- NULL
-  #path_expression$KO <- NULL
-  #path_expression$Pathway_code <- NULL
-  #path_expression$Pathway_name <- NULL
-  #path_expression$Pathway_family1 <- NULL
-  #path_expression$Pathway_family2 <- NULL
-  #path_expression$Relabund <- NULL
-  return(path_expression)
-}
-
-#--------------------------------------------------------------------------------------------------------------#
-
-# Prep all of the data - aggregating by pathways
-cef_expression <- format_expression_aggregate(cef)
-clinda_expression <- format_expression_aggregate(clinda)
-strep_expression <- format_expression_aggregate(strep)
-gf_expression <- format_expression_aggregate(gf)
-
-# Combine with other expression tables
-all_expression <- merge(cef_expression , clinda_expression, by = 'row.names')
-rownames(all_expression) <- all_expression$Row.names
-all_expression$Row.names <- NULL
-colnames(all_expression) <- c('cefoperazone', 'clindamycin')
-all_expression <- merge(all_expression , strep_expression, by = 'row.names')
-rownames(all_expression) <- all_expression$Row.names
-all_expression$Row.names <- NULL
-colnames(all_expression) <- c('cefoperazone', 'clindamycin', 'streptomycin')
-all_expression <- merge(all_expression , gf_expression, by = 'row.names')
-rownames(all_expression) <- all_expression$Row.names
-all_expression$Row.names <- NULL
-colnames(all_expression) <- c('Cefoperazone', 'Clindamycin', 'Streptomycin', 'Germfree')
-
-# Remove categories where all of the values are 0 and sort
-filter_all_expression <- all_expression[rowSums(all_expression[, -1]) > 0, ]
-filter_all_expression <- filter_all_expression[order(rowSums(filter_all_expression), decreasing = T), ]
-filter_all_expression <- as.matrix(filter_all_expression)
-
-# Define color palette
-color_pal <- rainbow(nrow(filter_all_expression))
-
-# Plot the final aggregated data
-pdf(file=figure_file, width=14, height=9)
-par(las=1, mar=c(2.2, 3, 1, 20), mgp=c(2, 0.8, 0), xpd=TRUE)
-barplot(filter_all_expression, col = color_pal, yaxt = 'n', ylim = c(-0.5, 100.5), ylab = '% Relative Abundance')
-axis(side = 2, at = seq(0, 100, 20), tick = TRUE)
-segments(x0 = c(0, 0, 5), y0 = c(100, 0, 100), x1 = c(5, 5, 5), y1 = c(100, 0, 0))
-segments(x0 = c(0, 0, 0, 0), y0 = c(20, 40, 60, 80), x1 = c(5, 5, 5, 5), y1 = c(20, 40, 60, 80), lty = 2)
-legend(5, 85, legend = rev(rownames(filter_all_expression)), pt.bg = rev(color_pal), pch = 22, pt.cex = 2, cex = 1)
-dev.off()
-
-#--------------------------------------------------------------------------------------------------------------#
 
 
-# Prep all of the data - no aggregation
-cef_expression_all <- format_expression(cef)
-clinda_expression_all <- format_expression(clinda)
-strep_expression_all <- format_expression(strep)
-gf_expression_all <- format_expression(gf)
+# Highly abundant
+par(mar=c(3,2,1,1))
+barchart()
 
-# Filter out the genes you aren't interested in
-#cef_expression_all_filter <- subset(cef_expression_all, Pathway_name == 'Microbial metabolism in diverse environments')
-#clinda_expression_all_filter <- subset(clinda_expression_all, Pathway_name == 'Microbial metabolism in diverse environments')
-#strep_expression_all_filter <- subset(strep_expression_all, Pathway_name == 'Microbial metabolism in diverse environments')
-#gf_expression_all_filter <- subset(gf_expression_all, Pathway_name == 'Microbial metabolism in diverse environments')
 
-# Remove useless columns
-cef_expression_all$Normalized_abundance <- NULL
-cef_expression_all$KO <- NULL
-cef_expression_all$Pathway_code <- NULL
-cef_expression_all$Pathway_name <- NULL
-cef_expression_all$Pathway_family1 <- NULL
-cef_expression_all$Pathway_family2 <- NULL
-cef_expression_all$Gene_code <- NULL
-cef_expression_all$Gene_name <- NULL
 
-clinda_expression_all$Normalized_abundance <- NULL
-clinda_expression_all$KO <- NULL
-clinda_expression_all$Pathway_code <- NULL
-clinda_expression_all$Pathway_name <- NULL
-clinda_expression_all$Pathway_family1 <- NULL
-clinda_expression_all$Pathway_family2 <- NULL
-clinda_expression_all$Gene_code <- NULL
-clinda_expression_all$Gene_name <- NULL
 
-strep_expression_all$Normalized_abundance <- NULL
-strep_expression_all$KO <- NULL
-strep_expression_all$Pathway_code <- NULL
-strep_expression_all$Pathway_name <- NULL
-strep_expression_all$Pathway_family1 <- NULL
-strep_expression_all$Pathway_family2 <- NULL
-strep_expression_all$Gene_code <- NULL
-strep_expression_all$Gene_name <- NULL
 
-gf_expression_all$Normalized_abundance <- NULL
-gf_expression_all$KO <- NULL
-gf_expression_all$Pathway_code <- NULL
-gf_expression_all$Pathway_name <- NULL
-gf_expression_all$Pathway_family1 <- NULL
-gf_expression_all$Pathway_family2 <- NULL
-gf_expression_all$Gene_code <- NULL
-gf_expression_all$Gene_name <- NULL
+
+
+
 
