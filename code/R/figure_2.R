b8f1809f52390d268b62609da44d93591fefd5c1
Matt Jenior <mljenior@umich.edu>
2016-06-18 00:50:42 -0400

    added barplot to figure 2 code

--- code/R/figure_2.R
+++ code/R/figure_2.R
@@ -1,5 +1,5 @@
 
-deps <- c('wesanderson');
+deps <- c('wesanderson','vegan');
 for (dep in deps){
   if (dep %in% installed.packages()[,"Package"] == FALSE){
     install.packages(as.character(dep), quiet=TRUE);
@@ -52,9 +52,13 @@ combined_mapping$Row.names <- NULL
 combined_mapping <- merge(combined_mapping, germfree, by='row.names')
 rownames(combined_mapping) <- combined_mapping$Row.names
 combined_mapping$Row.names <- NULL
+rm(cefoperazone, clindamycin, streptomycin, germfree)
+
+# Remove genes not in a pathway
+combined_mapping <- subset(combined_mapping, pathway != 'none')
 
 # Rarefy mappings to be equal within sequencing type
-sub_size <- round(min(colSums(combined_mapping[,1:4])) * 0.9) # 34050
+sub_size <- round(min(colSums(combined_mapping[,1:4])) * 0.9)
 combined_mapping$Cefoperazone <- t(rrarefy(combined_mapping$Cefoperazone, sample=sub_size))
 combined_mapping$Clindamycin <- t(rrarefy(combined_mapping$Clindamycin, sample=sub_size))
 combined_mapping$Streptomycin <- t(rrarefy(combined_mapping$Streptomycin, sample=sub_size))
@@ -63,9 +67,6 @@ combined_mapping$Germfree <- t(rrarefy(combined_mapping$Germfree, sample=sub_siz
 # Eliminate genes with no transcripts mapping
 combined_mapping <- combined_mapping[rowSums(combined_mapping[,1:4]) != 0, ] 
 
-# Clean up
-rm(cefoperazone, clindamycin, streptomycin, germfree, sub_size)
-
 # Remove unused catagories
 combined_mapping$ko <- NULL
 combined_mapping$gene <- NULL
@@ -73,83 +74,70 @@ combined_mapping$gene <- NULL
 #--------------------------------------------------------------------------------------------------------------#
 
 # Subset by KEGG catagory and pool
-
-# Metabolism - not working
 carbohydrate <- subset(combined_mapping, grepl('Carbohydrate_metabolism', combined_mapping$pathway))
-carbohydrate <- colSums(carbohydrate[,1:4])
+carbohydrate <- t(as.data.frame(colSums(carbohydrate[,1:4])))
 energy <- subset(combined_mapping, grepl('Energy_metabolism', combined_mapping$pathway))
-energy <- colSums(energy[,1:4])
+energy <- t(as.data.frame(colSums(energy[,1:4])))
 lipid <- subset(combined_mapping, grepl('Lipid_metabolism', combined_mapping$pathway))
-lipid <- colSums(lipid[,1:4])
+lipid <- t(as.data.frame(colSums(lipid[,1:4])))
 nucleotide <- subset(combined_mapping, grepl('Nucleotide_metabolism', combined_mapping$pathway))
-nucleotide <- colSums(nucleotide[,1:4])
+nucleotide <- t(as.data.frame(colSums(nucleotide[,1:4])))
 amino_acid <- subset(combined_mapping, grepl('Amino_acid_metabolism', combined_mapping$pathway))
-amino_acid <- colSums(amino_acid[,1:4])
+amino_acid <- t(as.data.frame(colSums(amino_acid[,1:4])))
 other_amino_acids <- subset(combined_mapping, grepl('Metabolism_of_other_amino_acids', combined_mapping$pathway))
-other_amino_acids <- colSums(other_amino_acids[,1:4])
+other_amino_acids <- t(as.data.frame(colSums(other_amino_acids[,1:4])))
 glycan <- subset(combined_mapping, grepl('Glycan_biosynthesis_and_metabolism', combined_mapping$pathway))
-glycan <- colSums(glycan[,1:4])
+glycan <- t(as.data.frame(colSums(glycan[,1:4])))
 cofactors_and_vitamins <- subset(combined_mapping, grepl('Metabolism_of_cofactors_and_vitamins', combined_mapping$pathway))
-cofactors_and_vitamins <- colSums(cofactors_and_vitamins[,1:4])
+cofactors_and_vitamins <- t(as.data.frame(colSums(cofactors_and_vitamins[,1:4])))
 terpenoids_and_polyketides <- subset(combined_mapping, grepl('Metabolism_of_terpenoids_and_polyketides', combined_mapping$pathway))
-terpenoids_and_polyketides <- colSums(terpenoids_and_polyketides[,1:4])
+terpenoids_and_polyketides <- t(as.data.frame(colSums(terpenoids_and_polyketides[,1:4])))
 secondary_metabolites <- subset(combined_mapping, grepl('Biosynthesis_of_other_secondary_metabolites', combined_mapping$pathway))
-secondary_metabolites <- colSums(secondary_metabolites[,1:4])
+secondary_metabolites <- t(as.data.frame(colSums(secondary_metabolites[,1:4])))
 xenobiotics <- subset(combined_mapping, grepl('Xenobiotics_biodegradation_and_metabolism', combined_mapping$pathway))
-xenobiotics <- colSums(xenobiotics[,1:4])
+xenobiotics <- t(as.data.frame(colSums(xenobiotics[,1:4])))
 genetics <- subset(combined_mapping, grepl('Genetic_Information_Processing', combined_mapping$pathway))
-genetics <- colSums(genetics[,1:4])
+genetics <- t(as.data.frame(colSums(genetics[,1:4])))
 motility <- subset(combined_mapping, grepl('Cell_motility', combined_mapping$pathway))
-motility <- colSums(motility[,1:4])
+motility <- t(as.data.frame(colSums(motility[,1:4])))
 rm(combined_mapping)
 
 # Assemble final table
 pooled_mapping <- rbind(carbohydrate, energy, lipid, nucleotide, amino_acid, other_amino_acids, glycan,
                         cofactors_and_vitamins, terpenoids_and_polyketides, secondary_metabolites,
                         xenobiotics, genetics, motility)
+rm(carbohydrate, energy, lipid, nucleotide, amino_acid, other_amino_acids, glycan, cofactors_and_vitamins, terpenoids_and_polyketides, secondary_metabolites, xenobiotics, genetics, motility)
+rownames(pooled_mapping) <- c('Carbohydrate metabolism','Energy metabolism','Lipid metabolism','Nucleotide metabolism',
+                              'Amino acid metabolism','Metabolism of other amino acids','Glycan biosynthesis and metabolism',
+                              'Metabolism of cofactors and vitamins','Metabolism of terpenoids and polyketides','Biosynthesis of other secondary metabolites',
+                              'Xenobiotics biodegradation and metabolism','Genetic Information Processing','Cell motility')
+normalized_mapping <- pooled_mapping
+transformed_mapping <- log10(pooled_mapping)
+rm(pooled_mapping)
 
 #--------------------------------------------------------------------------------------------------------------#
 
-# Pooling pathways
-cefoperazone_pooled_pathways <- aggregate(transcripts ~ pathway_name, FUN = sum, data = cefoperazone_pathways)  
-rownames(cefoperazone_pooled_pathways) <- cefoperazone_pooled_pathways[,1]
-cefoperazone_pooled_pathways[,1] <- NULL
-colnames(cefoperazone_pooled_pathways) <- 'Cefoperazone'
-
-
-
-
-
-
-#--------------------------------------------------------------------------------------------------------------#
-
-fox <- wes_palette("FantasticFox")
+darjeeling <- wes_palette("Darjeeling")
 plot_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/results/figures/figure_2.pdf'
 
 # Open a PDF
-pdf(file=plot_file, width=12, height=7)
+pdf(file=plot_file, width=12, height=5)
 
-# Create layout for multi-plot
-layout(mat=matrix(c(1,1, 
-                    2,2), 
-                  nrow=2, ncol=2, byrow=TRUE))
-
-# Lowly abundant
-par(mar=c(3,2,1,1))
-barchart()
-
-
-
-
-# Highly abundant
-par(mar=c(3,2,1,1))
-barchart()
+# Pathway abundance bar chart
+par(las=1, mar=c(6,4,1,1))
+barplot(t(transformed_mapping), col=c(darjeeling[1],darjeeling[2],darjeeling[4],darjeeling[5]), 
+        beside=TRUE, xaxt='n', yaxt='n', ylab='Transcript Abundance (Log10)', ylim=c(0,5))
+box()
+labelsY <- parse(text=paste(rep(10,4), '^', seq(1,4,1), sep=''))
+axis(side=2, at=c(1:4), labelsY, tick=TRUE, las=1)
+abline(h=c(1:8), lty=2)
+legend('topleft', legend=c('Cefoperazone', 'Clindamycin', 'Streptomycin', 'Gnotobiotic'), pt.cex=2, bty='n',
+       pch=22, col='black', pt.bg=c(darjeeling[1],darjeeling[2],darjeeling[4],darjeeling[5]), ncol=2)
 
 
 
+text(y = seq(0, 100, by=20), par("usr")[1], labels = rownames(transformed_mapping), srt = 45, pos = 2, xpd = TRUE, cex=0.8)
 
 
 
-
-
-
+dev.off()
