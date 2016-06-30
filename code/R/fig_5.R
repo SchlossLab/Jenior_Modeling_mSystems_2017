b629f95caff67cab7fd331fc68b700b5778b48cb
Matt Jenior <mljenior@umich.edu>
2016-06-30 15:38:05 -0400

    removed redundant R code and simplified figure script naming

--- code/R/fig_5.R
+++ code/R/fig_5.R
@@ -13,11 +13,13 @@ rm(dep, deps)
 # Define file variables for network plot
 network_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/data/metabolic_models/cefoperazone_630.bipartite.files/bipartite_graph.txt'
 ko_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/data/metabolic_models/cefoperazone_630.bipartite.files/cefoperazone_630.mapping.txt'
+layout_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/data/metabolic_models/optimal_layout.tsv'
 
 # Read in metabolic network data
 network <- read.table(network_file, header=FALSE, sep='\t')
 ko <- read.table(ko_file, header=TRUE, sep='\t')
-rm(network_file, ko_file)
+optimal_layout1 <- as.matrix(read.table(layout_file, header=TRUE, sep='\t', row.names=1))
+rm(network_file, ko_file, layout_file)
 
 # Format directed graph
 raw_graph <- graph.data.frame(network, directed=TRUE)
@@ -59,8 +61,6 @@ rm(ko, ko_simp, substrate_simp, node_size, mappings, nodes)
 V(largest_simple_graph)$color <- ifelse(grepl('K', V(largest_simple_graph)$name), adjustcolor('firebrick3', alpha.f=0.6), 'blue3') # Color nodes
 E(largest_simple_graph)$color <- 'gray15' # Color edges
 
-# Calculate optimal layout
-optimal <- layout.graphopt(graph=largest_simple_graph, niter=1000, charge=0.001, mass=50, spring.length=0, spring.constant=1)
 #-------------------------------------------------------------------------------------------------------------------------------------#
 
 # Set up example network
@@ -73,7 +73,7 @@ node_size <- matrix(c('K01', '10',
                       'K02', '45',
                       'K03', '50',
                       'C01', '20'), nrow=4, ncol=2, byrow=TRUE)
-optimal_layout <- matrix(c(-21.09826017, 22.1407060,
+optimal_layout2 <- matrix(c(-21.09826017, 22.1407060,
                            0.09077637, 0.2154631,
                            -8.32243732, -29.0949351,
                            29.67130628, 7.6231375), nrow=4, ncol=2, byrow=TRUE)
@@ -139,11 +139,11 @@ strep_only_importance <- strep_only_importance[order(strep_only_importance$Metab
 gf_only_importance <- gf_only_importance[order(gf_only_importance$Metabolite_score),]
 
 cef_only_importance$abx <- 'Cefoperazone'
-cef_only_importance$color <- 'blue2'
+cef_only_importance$color <- 'dodgerblue3'
 clinda_only_importance$abx <- 'Clindamycin'
 clinda_only_importance$color <- 'chartreuse4'
 strep_only_importance$abx <- 'Streptomycin'
-strep_only_importance$color <- 'firebrick3'
+strep_only_importance$color <- 'darkorange3'
 gf_only_importance$abx <- 'Gnotobiotic'
 gf_only_importance$color <- 'darkorchid4'
 
@@ -177,26 +177,27 @@ top_importances <- top_importances[ !(rownames(top_importances) %in% c('C11436')
 
 # Set up plotting environment
 plot_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/results/figures/figure_5.pdf'
-pdf(file=plot_file, width=20, height=8)
-layout(matrix(c(1,2), nrow=1, ncol=2, byrow=TRUE))
+pdf(file=plot_file, width=17, height=16)
+layout(matrix(c(1,2,
+                3,4), nrow=2, ncol=2, byrow=TRUE))
 
 #-------------------------------------------------------------------------------------------------------------------------------------#
 
 # Figure 4A - Large component of graph
-#par(mar=c(1,3,1,1))
-#plot(largest_simple_graph, vertex.label=NA, layout=optimal,
-#     edge.arrow.size=0.5, edge.arrow.width=0.8, vertex.frame.color='black')
-#legend(x=0.4, y=1.2, legend=c('KEGG Ortholog', 'Reaction Substrate'), 
-#       pt.bg=c('firebrick3', 'blue3'), col='black', pch=21, pt.cex=2.3)
-#legend(x=0.4, y=-0.8, legend=c('Total nodes: 1070', 'Enzyme nodes: 404', 'Substrate nodes: 666'), 
-#       pt.cex=0, text.font=c(2,1,1), bty='n')
-#mtext('A', side=2, line=2, las=2, adj=-2, padj=-16.5, cex=1.8)
+par(mar=c(1,3,1,1))
+plot(largest_simple_graph, vertex.label=NA, layout=optimal_layout1,
+     edge.arrow.size=0.5, edge.arrow.width=0.8, vertex.frame.color='black')
+legend(x=0.4, y=1.1, legend=c('KEGG Ortholog', 'Reaction Substrate'), 
+       pt.bg=c('firebrick3', 'blue3'), col='black', pch=21, pt.cex=2.3)
+legend(x=-1.2, y=-0.75, legend=c('Total nodes: 1070', 'Enzyme nodes: 404', 'Substrate nodes: 666'), 
+       pt.cex=0, text.font=c(2,1,1), bty='n')
+mtext('A', side=2, line=2, las=2, adj=-2, padj=-16.5, cex=1.8)
 
 #-------------------------------------------------------------------------------------------------------------------------------------#
 
-# Figure 4A - Example network and importance calculation
+# Figure 4B - Example network and importance calculation
 par(mar=c(0,1,0,0))
-plot(network, vertex.label=NA, layout=optimal_layout, vertex.frame.color='black', xlim=c(-1.2,1.2), ylim=c(-1.2,1.2))
+plot(network, vertex.label=NA, layout=optimal_layout2, vertex.frame.color='black', xlim=c(-1.2,1.2), ylim=c(-1.2,1.2))
 text(0.6, -0.8, expression(Importance == paste(log[2],'( ',frac(Sigma * t[i], e[o]),' ','-',' ',frac(Sigma * t[o], e[i]),' )')), cex = 1.7) # Importance algorithm
 text(x=-1, y=1.1, labels='Tetrathionate reductase', font=2) # Enzyme 1 name
 text(x=-1, y=1, labels='5', col='white') # Enzyme 1 transcription
@@ -206,25 +207,25 @@ text(x=1, y=0.75, labels='Thiosulfate Oxidase', font=2) # Enzyme 3
 text(x=0.99, y=0.44, labels='115', col='white', cex=2.1) # Enzyme 3 transcription
 text(x=c(0.1,0.1), y=c(-0.02,-0.12), labels=c('Thiosulfate','= 6.658'), cex=1.3, font=c(2,1)) # Compound & calculated importance
 segments(x0=-0.05, y0=-0.17, x1=0.25, y1=-0.17, lwd=2)
-mtext('A', side=2, line=2, las=2, adj=-2, padj=-16.5, cex=1.8)
+mtext('B', side=2, line=2, las=2, adj=-2, padj=-16.5, cex=1.8)
 
 #-------------------------------------------------------------------------------------------------------------------------------------#
 
-# Figure 4B - Top compound importances
+# Figure 4C - Top compound importances
 par(mar=c(4,3,1,1), xaxs='i')
 dotchart(top_importances$Metabolite_score, labels=top_importances$Compound_name, 
          lcolor=NA, cex=1.5, groups=top_importances$abx, color=top_importances$color, 
-         xlab='Metabolite Importance Score', xlim=c(0,25), gcolor="black", pch=19)
-segments(x0=rep(0, 14), y0=c(1:8, 11, 14, 17:20), x1=rep(25, 14), y1=c(1:8, 11, 14, 17:20), lty=2)
-mtext('B', side=2, line=2, las=2, adj=0.5, padj=-14.5, cex=1.8)
+         xlab='Metabolite Importance Score', xlim=c(0,12), gcolor="black", pch=19)
+segments(x0=rep(0, 14), y0=c(1:8, 11, 14, 17:20), x1=rep(12, 14), y1=c(1:8, 11, 14, 17:20), lty=2)
+mtext('C', side=2, line=2, las=2, adj=0.5, padj=-14.5, cex=1.8)
 
 #-------------------------------------------------------------------------------------------------------------------------------------#
 
-# Figure 4C - Growth on important compounds
+# Figure 4D - Growth on important compounds
 #par(mar=c(3,3,1,1), xaxs='i')
+#plot()
 
-
-#mtext('C', side=2, line=2, las=2, adj=0.5, padj=-14.5, cex=1.8)
+#mtext('D', side=2, line=2, las=2, adj=0.5, padj=-14.5, cex=1.8)
 
 #-------------------------------------------------------------------------------------------------------------------------------------#
 
