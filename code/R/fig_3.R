63b16bdb4448ba738321d2ebe82f3eeebf37c17d
Matt Jenior <mljenior@umich.edu>
2016-07-19 17:23:08 -0400

    working on figure 3

--- code/R/fig_3.R
+++ code/R/fig_3.R
@@ -1,171 +1,188 @@
 
-deps <- c('vegan', 'wesanderson');
+deps <- c('wesanderson','vegan', 'matrixStats');
 for (dep in deps){
   if (dep %in% installed.packages()[,"Package"] == FALSE){
     install.packages(as.character(dep), quiet=TRUE);
-  } 
+  }
   library(dep, verbose=FALSE, character.only=TRUE)
 }
 rm(dep, deps)
 
-# Import and format transcript mapping data
-cefoperazone_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/data/mapping/cdifficile630/cefoperazone_630.RNA_reads2cdf630.norm.annotated.txt'
-clindamycin_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/data/mapping/cdifficile630/clindamycin_630.RNA_reads2cdf630.norm.annotated.txt'
-streptomycin_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/data/mapping/cdifficile630/streptomycin_630.RNA_reads2cdf630.norm.annotated.txt'
-germfree_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/data/mapping/cdifficile630/germfree.RNA_reads2cdf630.norm.annotated.txt'
+#--------------------------------------------------------------------------------------------------------------#
 
-# Load in data
-cefoperazone <- read.delim(cefoperazone_file, sep='\t', header=FALSE, row.names=1)
-colnames(cefoperazone) <- c('Cefoperazone', 'ko', 'gene', 'pathway')
-clindamycin <- read.delim(clindamycin_file, sep='\t', header=FALSE, row.names=1)
-colnames(clindamycin) <- c('Clindamycin', 'ko', 'gene', 'pathway')
-streptomycin <- read.delim(streptomycin_file, sep='\t', header=FALSE, row.names=1)
-colnames(streptomycin) <- c('Streptomycin', 'ko', 'gene', 'pathway')
-germfree <- read.delim(germfree_file, sep='\t', header=FALSE, row.names=1)
-colnames(germfree) <- c('Germfree', 'ko', 'gene', 'pathway')
+# Define variables
+cefoperazone_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/data/mapping/cdifficile630/select_genes/cefoperazone_630.RNA_reads2select.all.norm.txt'
+clindamycin_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/data/mapping/cdifficile630/select_genes/clindamycin_630.RNA_reads2select.all.norm.txt'
+streptomycin_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/data/mapping/cdifficile630/select_genes/streptomycin_630.RNA_reads2select.all.norm.txt'
+germfree_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/data/mapping/cdifficile630/select_genes/germfree.RNA_reads2select.all.norm.txt'
+
+# Open files
+cefoperazone <- read.delim(cefoperazone_file, sep='\t', header=FALSE)
+colnames(cefoperazone) <- c('gene', 'Cefoperazone')
+clindamycin <- read.delim(clindamycin_file, sep='\t', header=FALSE)
+colnames(clindamycin) <- c('gene', 'Clindamycin')
+streptomycin <- read.delim(streptomycin_file, sep='\t', header=FALSE)
+colnames(streptomycin) <- c('gene', 'Streptomycin')
+germfree <- read.delim(germfree_file, sep='\t', header=FALSE)
+colnames(germfree) <- c('gene', 'Germfree')
+
+# Clean up
 rm(cefoperazone_file, clindamycin_file, streptomycin_file, germfree_file)
 
-#-------------------------------------------------------------------------------------------------------------------------------------#
-
-# Format data for merging
-cefoperazone$ko <- NULL
-cefoperazone$gene <- NULL
-cefoperazone$pathway <- NULL
-clindamycin$ko <- NULL
-clindamycin$gene <- NULL
-clindamycin$pathway <- NULL
-streptomycin$ko <- NULL
-streptomycin$gene <- NULL
-streptomycin$pathway <- NULL
+#--------------------------------------------------------------------------------------------------------------#
 
 # Merge tables
-combined_mapping <- merge(cefoperazone, clindamycin, by='row.names')
-rownames(combined_mapping) <- combined_mapping$Row.names
-combined_mapping$Row.names <- NULL
-combined_mapping <- merge(combined_mapping, streptomycin, by='row.names')
-rownames(combined_mapping) <- combined_mapping$Row.names
-combined_mapping$Row.names <- NULL
-combined_mapping <- merge(combined_mapping, germfree, by='row.names')
-rownames(combined_mapping) <- combined_mapping$Row.names
-combined_mapping$Row.names <- NULL
+combined_mapping <- merge(cefoperazone, clindamycin, by='gene')
+combined_mapping <- merge(combined_mapping, streptomycin, by='gene')
+combined_mapping$gene <- gsub("Clostridium_difficile_630\\|","", combined_mapping$gene)
+combined_mapping$gene <- gsub('ENA\\|CDT20869\\|CDT20869.1\\|Clostridium_difficile_putative_phage_replication_protein_','', combined_mapping$gene)
+combined_mapping$gene <- gsub('_',' ', combined_mapping$gene)
+rownames(combined_mapping) <- combined_mapping$gene
+combined_mapping$gene <- NULL
 rm(cefoperazone, clindamycin, streptomycin, germfree)
 
-# Rarefy mappings
-sub_size <- round(min(colSums(combined_mapping[,1:4])) * 0.9) # 30645
-combined_mapping$Cefoperazone <- t(rrarefy(combined_mapping$Cefoperazone, sample=sub_size))
-combined_mapping$Clindamycin <- t(rrarefy(combined_mapping$Clindamycin, sample=sub_size))
-combined_mapping$Streptomycin <- t(rrarefy(combined_mapping$Streptomycin, sample=sub_size))
-combined_mapping$Germfree <- t(rrarefy(combined_mapping$Germfree, sample=sub_size))
-rm(sub_size)
+# Iteratively rarefy mappings
+sub_size <- round(min(colSums(combined_mapping[,1:3])) * 0.9) # 2668
+cefoperazone <- t(rrarefy(combined_mapping$Cefoperazone, sample=sub_size))
+clindamycin <- t(rrarefy(combined_mapping$Clindamycin, sample=sub_size))
+streptomycin <- t(rrarefy(combined_mapping$Streptomycin, sample=sub_size))
+for (index in 1:1000) {
+  cefoperazone <- cbind(cefoperazone, t(rrarefy(combined_mapping$Cefoperazone, sample=sub_size)))
+  clindamycin <- cbind(clindamycin, t(rrarefy(combined_mapping$Clindamycin, sample=sub_size)))
+  streptomycin <- cbind(streptomycin, t(rrarefy(combined_mapping$Streptomycin, sample=sub_size)))
+}
+# Medians
+combined_mapping$Cefoperazone <- rowMedians(cefoperazone)
+combined_mapping$Clindamycin <- rowMedians(clindamycin)
+combined_mapping$Streptomycin <- rowMedians(streptomycin)
+normalized_mapping <- combined_mapping
+# SDs
+combined_mapping$Cefoperazone <- rowSds(cefoperazone)
+combined_mapping$Clindamycin <- rowSds(clindamycin)
+combined_mapping$Streptomycin <- rowSds(streptomycin)
+sd_mapping <- combined_mapping
+# Clean up
+rm(sub_size, cefoperazone, clindamycin, streptomycin, combined_mapping, index)
 
-#-------------------------------------------------------------------------------------------------------------------------------------#
+# Log10 transform the data
+normalized_mapping[normalized_mapping == 0] <- 1
+transformed_mapping <- log10(normalized_mapping)
+rm(normalized_mapping)
 
-# Select genes associated with lifecycle and virulence
+#--------------------------------------------------------------------------------------------------------------#
 
-# Sporulation associated genes
-spo <- rbind(subset(combined_mapping, grepl('spo.....;', combined_mapping$gene)), # 
-             subset(combined_mapping, grepl('spo....;', combined_mapping$gene)),
-             subset(combined_mapping, grepl('spo...;', combined_mapping$gene)),
-             subset(combined_mapping, grepl('spo..;', combined_mapping$gene)))
-sig <- subset(combined_mapping, grepl('sig.;', combined_mapping$gene))
-sip <- subset(combined_mapping, grepl('sip.;', combined_mapping$gene))
-spm <- subset(combined_mapping, grepl('spm.;', combined_mapping$gene))
-oxa <- subset(combined_mapping, grepl('oxa..;', combined_mapping$gene))
-cot <- subset(combined_mapping, grepl('cot...;', combined_mapping$gene))
-bcl <- subset(combined_mapping, grepl('bcl..;', combined_mapping$gene))
-ssp <- subset(combined_mapping, grepl('ssp.;', combined_mapping$gene))
-CD630_01250 <- subset(combined_mapping, grepl('CD630_01250', rownames(combined_mapping)))
-CD630_01290 <- subset(combined_mapping, grepl('CD630_01290', rownames(combined_mapping)))
-CD630_05960 <- subset(combined_mapping, grepl('CD630_05960', rownames(combined_mapping)))
-CD630_10450 <- subset(combined_mapping, grepl('CD630_10450', rownames(combined_mapping)))
-CD630_11680 <- subset(combined_mapping, grepl('CD630_11680', rownames(combined_mapping)))
-CD630_12210 <- subset(combined_mapping, grepl('CD630_12210', rownames(combined_mapping)))
-CD630_12900 <- subset(combined_mapping, grepl('CD630_12900', rownames(combined_mapping)))
-CD630_12980 <- subset(combined_mapping, grepl('CD630_12980', rownames(combined_mapping)))
-CD630_13210 <- subset(combined_mapping, grepl('CD630_13210', rownames(combined_mapping)))
-CD630_14330 <- subset(combined_mapping, grepl('CD630_14330', rownames(combined_mapping)))
-CD630_15110 <- subset(combined_mapping, grepl('CD630_15110', rownames(combined_mapping)))
-CD630_16130 <- subset(combined_mapping, grepl('CD630_16130', rownames(combined_mapping)))
-CD630_21440 <- subset(combined_mapping, grepl('CD630_21440', rownames(combined_mapping)))
-CD630_23760 <- subset(combined_mapping, grepl('CD630_23760', rownames(combined_mapping)))
-CD630_26410 <- subset(combined_mapping, grepl('CD630_26410', rownames(combined_mapping)))
-CD630_26850 <- subset(combined_mapping, grepl('CD630_26850', rownames(combined_mapping)))
-CD630_34640 <- subset(combined_mapping, grepl('CD630_34640', rownames(combined_mapping)))
-CD630_35630 <- subset(combined_mapping, grepl('CD630_35630', rownames(combined_mapping)))
-CD630_35690 <- subset(combined_mapping, grepl('CD630_35690', rownames(combined_mapping)))
-sporulation_genes <- rbind(spo, sig, sip, spm, oxa, cot, bcl, ssp, CD630_01250, CD630_01290, 
-                           CD630_05960, CD630_10450, CD630_11680, CD630_12210, CD630_12900, 
-                           CD630_12980, CD630_13210, CD630_14330, CD630_15110, CD630_16130, 
-                           CD630_21440, CD630_23760, CD630_26410, CD630_26850, CD630_34640, 
-                           CD630_35630, CD630_35690)
-sporulation_sums <- colSums(sporulation_genes[,c(1:4)])
-sporulation_genes$pathway <- NULL
-sporulation_genes$ko <- NULL
-rm(spo, sig, sip, spm, oxa, cot, bcl, ssp, CD630_01250, CD630_01290, 
-      CD630_05960, CD630_10450, CD630_11680, CD630_12210, CD630_12900, 
-      CD630_12980, CD630_13210, CD630_14330, CD630_15110, CD630_16130, 
-      CD630_21440, CD630_23760, CD630_26410, CD630_26850, CD630_34640, 
-      CD630_35630, CD630_35690)
+# Subset for sigma factors
+sigma_keep <- c('CodY','CcpA','CdtR','SigH','SigB','SigA1','SigA2','SigE','SigF','SigG','SigK','SigV','FliA',
+           'TetR_family|1','TetR_family|2','TetR_family|3','TetR_family|4','TetR_family|5','TetR_family|6',
+           'TetR_family|7','TetR_family|8','TetR_family|9','TetR_family|10','TetR_family|11','TetR_family|12',
+           'TetR_family|13','TetR_family|14','TetR_family|15','TetR_family|16','Rex','PrdR','Spo0A','RstA','DpaA')
+sigma_medians <- subset(transformed_mapping, rownames(transformed_mapping) %in% sigma_keep)
+sigma_sds <- subset(transformed_mapping, rownames(transformed_mapping) %in% sigma_keep)
 
+# Subset for PaLoc
+paloc_keep <- c('TcdR','TcdC','TcdE','CdtR','TcdA','TcdB')
+paloc_medians <- subset(transformed_mapping, rownames(transformed_mapping) %in% paloc_keep)
+paloc_sds <- subset(sd_mapping, rownames(sd_mapping) %in% paloc_keep) * 3
 
-# Toxin associated genes
-tcd <- subset(combined_mapping, grepl('tcd.;', combined_mapping$gene))
-cdt <- subset(combined_mapping, grepl('cdt.;', combined_mapping$gene))
-toxin_genes <- rbind(tcd, cdt)
-toxin_sums <- colSums(toxin_genes[,c(1:4)])
-toxin_genes$pathway <- NULL
-toxin_genes$ko <- NULL
-rm(tcd, cdt, combined_mapping)
+# Subset for sporulation
+sporulation_keep <- c('SpoIID','SpoIIID','SpoIIAA','SpoIIAB','SpoIIIAA','SpoIIIAB','SpoIIIAC','SpoIIIAD',
+                 'SpoIIIAE','SpoIIIAG','SpoIIIAH','SpoIIP','SpoIIGA','SpoIIE','SpoIIR','SpoVAC','SpoVAD',
+                 'SpoVAE','SpoIVB2','SpoIVB','SpoVS','SpoIV','SpoIVA','SpoVE','SpoVD','SpoVFB','SpoVFA','SpoVB',
+                 'SpoVT','SpoVG','CD1579','CD1492','CD2492','CotF','CotCB','CdeC','CotA','SodA','CotJB2','CotD',
+                 'Gpr','SspA','BclA2','SspB','BclA3')
+sporulation_medians <- subset(transformed_mapping, rownames(transformed_mapping) %in% sporulation_keep)
+sporulation_sds <- subset(sd_mapping, rownames(sd_mapping) %in% sporulation_keep)
 
-# Assemble full gene table
-select_mapping <- rbind(sporulation_genes, toxin_genes)
-select_mapping[select_mapping == 0] <- 1
-transformed_mapping <- log10(select_mapping[,c(1:4)])
-transformed_mapping$gene <- select_mapping$gene
-rm(select_mapping, sporulation_genes, toxin_genes)
+# Quorum sensing
+quorum_keep <- c('LuxS','AgrD','AgrB')
+quorum_medians <- subset(transformed_mapping, rownames(transformed_mapping) %in% quorum_keep)
+quorum_sds <- subset(sd_mapping, rownames(sd_mapping) %in% quorum_keep)
 
-# Assemble summary table
-sum_mapping <- log10(rbind(sporulation_sums, toxin_sums))
-rm(sporulation_sums, toxin_sums)
+# Clean up
+rm(transformed_mapping, sigma_keep, paloc_keep, sporulation_keep, quorum_keep)
 
-#-------------------------------------------------------------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------------------------------------#
 
-# Set up plot
+# Set the color palette and plotting environment
+select_palette <- c(wes_palette("FantasticFox")[1], wes_palette("FantasticFox")[3], wes_palette("FantasticFox")[5])
 plot_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/results/figures/figure_3.pdf'
-darjeeling <- wes_palette("Darjeeling")
-pdf(file=plot_file, width=14, height=12)
-layout(matrix(c(1,2), nrow=2, ncol=1, byrow = TRUE))
+pdf(file=plot_file, width=12, height=14)
+layout(matrix(c(1,1,
+                2,2,
+                3,4),
+              nrow=3, ncol=2, byrow = TRUE))
 
-#-------------------------------------------------------------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------------------------------------#
 
-# Plot all genes
-par(las=1, mar=c(8,4,1,1))
-barplot(t(transformed_mapping[,c(1:4)]), col=c(darjeeling[1],darjeeling[2],darjeeling[4],darjeeling[5]), 
-        beside=TRUE, xaxt='n', yaxt='n', ylab='Transcript Abundance (Log10)', ylim=c(0,3))
+# A - Sigma factors
+par(las=1, mar=c(4,4,1,1), mgp=c(2.5, 1, 0))
+x_coords <- barplot(t(sigma_medians), col=select_palette, space=c(0,1.5), beside=TRUE, xaxt='n', yaxt='n', 
+        ylab=expression(paste('Transcript Abundance (',Log[10],')')), ylim=c(0,3))
 box()
-axis(side=2, at=c(1:4), parse(text=paste(rep(10,3), '^', seq(1,3,1), sep='')), tick=TRUE, las=1)
+axis(side=2, at=c(1:4), parse(text=paste(rep(10,4), '^', seq(1,4,1), sep='')), tick=TRUE, las=1, cex=1.4)
 abline(h=c(1:4), lty=2)
-legend('topleft', legend=c('Cefoperazone', 'Clindamycin', 'Streptomycin', 'Gnotobiotic'), pt.cex=2, bty='n',
-       pch=22, col='black', pt.bg=c(darjeeling[1],darjeeling[2],darjeeling[4],darjeeling[5]), ncol=2)
-mtext('A', side=2, line=2, las=2, adj=2, padj=-6.2, cex=1.5)
+legend('topleft', legend=c('Cefoperazone', 'Clindamycin', 'Streptomycin'), pt.cex=2.3, bty='n', cex=1.2,
+       pch=22, col='black', pt.bg=select_palette, ncol=1)
+text(x=seq(3.7,83.7,4.5), y=par()$usr[3]-0.03*(par()$usr[4]-par()$usr[3]),
+     labels=rownames(sigma_medians), srt=45, adj=1, xpd=TRUE, cex=1.2)
+legend('topright', legend='Sigma factors', pt.cex=0, bty='n', cex=1.8)
 
-#-------------------------------------------------------------------------------------------------------------------------------------#
+# 95% confidence interval
+x_coords <- as.data.frame(t(x_coords))
+colnames(x_coords) <- c('Cefoperazone', 'Clindamycin', 'Streptomycin')
+segments(x0=x_coords$Cefoperazone, y0=c(sigma_medians$Cefoperazone+sigma_sds$Cefoperazone), x1=x_coords$Cefoperazone, y1=c(sigma_medians$Cefoperazone-sigma_sds$Cefoperazone), lwd=1.2)
+segments(x0=x_coords$Clindamycin, y0=c(sigma_medians$Clindamycin+sigma_sds$Clindamycin), x1=x_coords$Clindamycin, y1=c(sigma_medians$Clindamycin-sigma_sds$Clindamycin), lwd=1.2)
+segments(x0=x_coords$Streptomycin, y0=c(sigma_medians$Streptomycin+sigma_sds$Streptomycin), x1=x_coords$Streptomycin, y1=c(sigma_medians$Streptomycin-sigma_sds$Streptomycin), lwd=1.2)
 
-# Plot catagory sums
-par(las=1, mar=c(3,4,1,1))
-barplot(t(sum_mapping), col=c(darjeeling[1],darjeeling[2],darjeeling[4],darjeeling[5]), 
-        beside=TRUE, xaxt='n', yaxt='n', ylab='Transcript Abundance (Log10)', ylim=c(0,4))
+#--------------------------------------------------------------------------------------------------------------#
+
+# B - Sporulation
+par(las=1, mar=c(4,4,1,1), mgp=c(2.5, 1, 0))
+barplot(t(sporulation_medians), col=select_palette, space=c(0,1.5),  beside=TRUE, xaxt='n', yaxt='n', 
+        ylab=expression(paste('Transcript Abundance (',Log[10],')')), ylim=c(0,3))
 box()
-axis(side=2, at=c(1:4), parse(text=paste(rep(10,4), '^', seq(1,4,1), sep='')), tick=TRUE, las=1)
+axis(side=2, at=c(1:4), parse(text=paste(rep(10,4), '^', seq(1,4,1), sep='')), tick=TRUE, las=1, cex=1.4)
 abline(h=c(1:4), lty=2)
-legend('topleft', legend=c('Cefoperazone', 'Clindamycin', 'Streptomycin', 'Gnotobiotic'), pt.cex=2, bty='n',
-       pch=22, col='black', pt.bg=c(darjeeling[1],darjeeling[2],darjeeling[4],darjeeling[5]), ncol=2)
-text(x=c(3.5,8.5), y=c(-0.2,-0.2), labels=c('Sporulation','Toxin Production'), adj=1, xpd=TRUE, cex=1.4)
-mtext('B', side=2, line=2, las=2, adj=2, padj=-6.2, cex=1.5)
+legend('topleft', legend=c('Cefoperazone', 'Clindamycin', 'Streptomycin'), pt.cex=2.3, bty='n', cex=1.2,
+       pch=22, col='black', pt.bg=select_palette, ncol=1)
+text(x=seq(3.7,205.2,4.5), y=par()$usr[3]-0.03*(par()$usr[4]-par()$usr[3]),
+     labels=rownames(sporulation_medians), srt=45, adj=1, xpd=TRUE, cex=0.8)
+legend('topright', legend='Sporulation', pt.cex=0, bty='n', cex=1.8)
 
-#-------------------------------------------------------------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------------------------------------#
+
+# C - PaLoc
+par(las=1, mar=c(4,4,1,1), mgp=c(2.5, 1, 0))
+barplot(t(paloc_medians), col=select_palette, space=c(0,1.5),  beside=TRUE, xaxt='n', yaxt='n', 
+        ylab=expression(paste('Transcript Abundance (',Log[10],')')), ylim=c(0,2))
+box()
+axis(side=2, at=c(1:4), parse(text=paste(rep(10,4), '^', seq(1,4,1), sep='')), tick=TRUE, las=1, cex=1.4)
+abline(h=c(1:4), lty=2)
+legend('topleft', legend=c('Cefoperazone', 'Clindamycin', 'Streptomycin'), pt.cex=2.3, bty='n', cex=1.2,
+       pch=22, col='black', pt.bg=select_palette, ncol=1)
+text(x=seq(3.7,29.7,4.5), y=par()$usr[3]-0.03*(par()$usr[4]-par()$usr[3]),
+     labels=rownames(paloc_medians), srt=45, adj=1, xpd=TRUE, cex=1.4)
+legend('topright', legend='Pathogenicity Locus', pt.cex=0, bty='n', cex=1.8)
+
+#--------------------------------------------------------------------------------------------------------------#
+
+# D - Quorum sensing
+par(las=1, mar=c(4,4,1,1), mgp=c(2.5, 1, 0))
+barplot(t(quorum_medians), col=select_palette, beside=TRUE, xaxt='n', yaxt='n', 
+        ylab=expression(paste('Transcript Abundance (',Log[10],')')), ylim=c(0,2))
+box()
+axis(side=2, at=c(1:4), parse(text=paste(rep(10,4), '^', seq(1,4,1), sep='')), tick=TRUE, las=1, cex=1.4)
+abline(h=c(1:4), lty=2)
+legend('topleft', legend=c('Cefoperazone', 'Clindamycin', 'Streptomycin'), pt.cex=2.3, bty='n', cex=1.2,
+       pch=22, col='black', pt.bg=select_palette, ncol=1)
+text(x=c(2.7,6.7,10.7), y=par()$usr[3]-0.03*(par()$usr[4]-par()$usr[3]),
+     labels=rownames(quorum_medians), srt=45, adj=1, xpd=TRUE, cex=1.6)
+legend('topright', legend='Quorum sensing', pt.cex=0, bty='n', cex=1.8)
+
+#--------------------------------------------------------------------------------------------------------------#
 
 # Clean up
 dev.off()
-rm(transformed_mapping, sum_mapping, darjeeling, plot_file)
+rm(paloc, quorum, sigma, sporulation, plot_file, select_palette)
+
 
