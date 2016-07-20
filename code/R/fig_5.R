e2bacc975503b9a379cf50da87ab2f40c91317c9
Matt Jenior <mljenior@umich.edu>
2016-07-20 14:30:10 -0400

    added code outline for figure 5

---
+++ code/R/fig_5.R
@@ -0,0 +1,48 @@
+
+deps <- c('shape', 'wesanderson', 'dplyr', 'NMF');
+for (dep in deps){
+  if (dep %in% installed.packages()[,"Package"] == FALSE){
+    install.packages(as.character(dep), quiet=TRUE);
+  } 
+  library(dep, verbose=FALSE, character.only=TRUE)
+}
+rm(dep, deps)
+
+# Select files
+
+
+# Read in data
+
+
+#-------------------------------------------------------------------------------------------------------------------------------------#
+
+# Format the data
+
+
+#-------------------------------------------------------------------------------------------------------------------------------------#
+
+# Set up multi-panel figure
+plot_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/results/figures/figure_5.pdf'
+pdf(file=plot_file, width=12, height=14)
+layout(matrix(c(1,1,
+                1,1,
+                2,2),
+              nrow=3, ncol=2, byrow = TRUE))
+
+#-------------------------------------------------------------------------------------------------------------------------------------#
+
+# A - Heatmap of metabolites
+
+
+#-------------------------------------------------------------------------------------------------------------------------------------#
+
+# B - Bar chart comparison of specific compounds
+
+
+#-------------------------------------------------------------------------------------------------------------------------------------#
+
+#Clean up
+dev.off()
+rm()
+
+
