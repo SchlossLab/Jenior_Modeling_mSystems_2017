f997450d087bd774d8069bc2b554bb932dffdcf4
Matt Jenior <mljenior@umich.edu>
2016-09-13 20:26:10 -0400

    Update fig_1.R
    added figure labels

--- code/R/figures/fig_1.R
+++ code/R/figures/fig_1.R
@@ -42,16 +42,18 @@ segments(x0=c(-4,0,2,2.75), y0=c(3.5,3.5,3.5,3.5), x1=c(-4,0,2,2.75), y1=c(2.5,2
 segments(x0=c(-4,-3,-2,-1,1), y0=c(3.25,3.25,3.25,3.25,3.25), x1=c(-4,-3,-2,-1,1), y1=c(2.75,2.75,2.75,2.75,2.75), lwd=2)
 points(x=c(2,2.75), y=c(4,4), pch=25, bg=c('white','black'), col='black', cex=2.5)
 text(x=c(-4,0,2,2.75), y=c(2.2,2.2,2.2,2.2), c('Day -7', 'Day -2', 'Day 0', '18 hrs'), cex=0.9)
-text(x=-4.6, y=3.2, 'Cefoperazone', cex=0.7)
-text(x=-4.6, y=2.95, 'or', font=2, cex=0.8)
-text(x=-4.6, y=2.7, 'Streptomycin', cex=0.7)
+#text(x=-4.6, y=3.2, 'Cefoperazone', cex=0.7)
+#text(x=-4.6, y=2.95, 'or', font=2, cex=0.8)
+#text(x=-4.6, y=2.7, 'Streptomycin', cex=0.7)
+text(x=-4.6, y=2.95, 'A', font=2, cex=2)
 
 # IP injection abx timeline
 Arrows(x0=-4, y0=0, x1=-1.5, y1=0, lwd=4, arr.type='triangle', arr.length=0.6, arr.width=0.2)
 segments(x0=c(-4,-3,-2.25), y0=c(-0.5,-0.5,-0.5), x1=c(-4,-3,-2.25), y1=c(0.5,0.5,0.5), lwd=4)
 points(x=c(-4,-3,-2.25), y=c(1,1,1), pch=c(25,25,25), bg=c('gray60','white','black'), col='black', cex=2.5)
 text(x=c(-4,-3,-2.25), y=c(-0.8,-0.8,-0.8), c('Day -1', 'Day 0', '18 hrs'), cex=0.9)
-text(x=-4.6, y=0, 'Clindamycin', cex=0.7)
+#text(x=-4.6, y=0, 'Clindamycin', cex=0.7)
+text(x=-4.6, y=0, 'B', cex=2)
 
 # Legend
 #legend(x=0, y=1.3, legend=expression('Antibiotic in Drinking Water', 'IP Injection of Antibiotic', paste(italic('C. difficile'), ' Spore Gavage'), 'Euthanize & Necropsy'), 
