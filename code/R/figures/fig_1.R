45f789030e9801c8475c0a034fee00a689f8bbe2
Matt Jenior <mljenior@umich.edu>
2016-10-06 17:53:25 -0400

    updated fig 1 to match rest of paper

--- code/R/figures/fig_1.R
+++ code/R/figures/fig_1.R
@@ -1,6 +1,6 @@
 
 # Load dependencies
-deps <- c('shape', 'plotrix');
+deps <- c('shape', 'plotrix', 'wesanderson', 'grid', 'gridExtra');
 for (dep in deps){
   if (dep %in% installed.packages()[,"Package"] == FALSE){
     install.packages(as.character(dep), quiet=TRUE);
@@ -10,45 +10,87 @@ for (dep in deps){
 
 #-------------------------------------------------------------------------------------------------------------------------------------#
 
-plot_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/results/figures/figure_1.pdf'
-pdf(file=plot_file, width=8, height=4)
+# Read in and format antibiotic table
+abx_table_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/data/abx.tsv'
+abx_table <- read.delim(abx_table_file, sep='\t', header=TRUE)
+
+abx_table[,2] <- gsub('_', ' ', abx_table[,2])
+abx_table[,3] <- gsub('_', ' ', abx_table[,3])
+abx_table[,4] <- gsub('_', ' ', abx_table[,4])
+abx_table[,5] <- gsub('_', ' ', abx_table[,5])
+abx_table[,6] <- gsub('_', ' ', abx_table[,6])
+
+abx_table$Target <- sapply(lapply(abx_table$Target, strwrap, width=40), paste, collapse="\n")
+abx_table$Activity <- sapply(lapply(abx_table$Activity, strwrap, width=40), paste, collapse="\n")
 
 #-------------------------------------------------------------------------------------------------------------------------------------#
 
-# Timeline of mouse experiments
-par(mar=c(0,3,0,0))
-plot(0, type='n', axes=F, xlab='', ylab='', xlim=c(-4.8,4), ylim=c(-2,5))
+plot_file <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/results/figures/figure_1.pdf'
+pdf(file=plot_file, width=7, height=10)
 
-# Abx in drinking water timeline
-rect(xleft=-4, ybottom=2.8, xright=0, ytop=3.2, col='darkorchid2', border='black')
-Arrows(x0=-4, y0=3, x1=3.5, y1=3, lwd=4, arr.type='triangle', arr.length=0.6, arr.width=0.2)
-segments(x0=c(-4,0,2,2.75), y0=c(3.5,3.5,3.5,3.5), x1=c(-4,0,2,2.75), y1=c(2.5,2.5,2.5,2.5), lwd=4)
-segments(x0=c(-4,-3,-2,-1,1), y0=c(3.25,3.25,3.25,3.25,3.25), x1=c(-4,-3,-2,-1,1), y1=c(2.75,2.75,2.75,2.75,2.75), lwd=2)
-points(x=c(2,2.75), y=c(4,4), pch=25, bg=c('white','black'), col='black', cex=2.5)
-text(x=c(-4,0,2,2.75), y=c(2.2,2.2,2.2,2.2), c('Day -7', 'Day -2', 'Day 0', '18 hrs'), cex=0.9)
-#text(x=-4.6, y=3.2, 'Cefoperazone', cex=0.7)
-#text(x=-4.6, y=2.95, 'or', font=2, cex=0.8)
-#text(x=-4.6, y=2.7, 'Streptomycin', cex=0.7)
-text(x=-4.6, y=2.95, 'a', cex=2)
+# Create layout for multi-plot
+layout(mat=matrix(c(1,
+                    2,
+                    3,
+                    4), nrow=4, ncol=1, byrow=TRUE))
 
-# IP injection abx timeline
-Arrows(x0=-4, y0=0, x1=-1.5, y1=0, lwd=4, arr.type='triangle', arr.length=0.6, arr.width=0.2)
-segments(x0=c(-4,-3,-2.25), y0=c(-0.5,-0.5,-0.5), x1=c(-4,-3,-2.25), y1=c(0.5,0.5,0.5), lwd=4)
-points(x=c(-4,-3,-2.25), y=c(1,1,1), pch=c(25,25,25), bg=c('chocolate1','white','black'), col='black', cex=2.5)
-text(x=c(-4,-3,-2.25), y=c(-0.8,-0.8,-0.8), c('Day -1', 'Day 0', '18 hrs'), cex=0.9)
-#text(x=-4.6, y=0, 'Clindamycin', cex=0.7)
-text(x=-4.6, y=0, 'b', cex=2)
+#-------------------------------------------------------------------------------------------------------------------------------------#
+
+par(mar=c(0,0,0,0))
+plot(0, type='n', axes=F, xlab='', ylab='', xlim=c(-5,5), ylim=c(-2,2)) # Empty plot
+
+# Strep in drinking water timeline
+rect(xleft=-4, ybottom=-0.5, xright=0, ytop=0.5, col=wes_palette("FantasticFox")[1], border='black')
+Arrows(x0=-4, y0=0, x1=4, y1=0, lwd=4, arr.type='triangle', arr.length=0.6, arr.width=0.2)
+segments(x0=c(-4,0,2,2.75), y0=c(0.5,0.5,0.5,0.5), x1=c(-4,0,2,2.75), y1=c(-0.5,-0.5,-0.5,-0.5), lwd=4)
+segments(x0=c(-4,-3,-2,-1,1), y0=c(0.25,0.25,0.25,0.25,0.25), x1=c(-4,-3,-2,-1,1), y1=c(-0.25,-0.25,-0.25,-0.25,-0.25), lwd=2)
+points(x=c(0,2,2.75), y=c(0.9,0.9,0.9), pch=25, bg=c('white','darkorchid3','black'), col='black', cex=3.4)
+text(x=c(-4,0,2,2.75), y=c(-0.8,-0.8,-0.8,-0.8), c('Day -7', 'Day -2', 'Day 0', '18 hrs'), cex=1.3)
+text(x=-4.6, y=0, 'a', cex=2, font=2)
+
+#----------------------------#
+
+par(mar=c(0,0,0,0))
+plot(0, type='n', axes=F, xlab='', ylab='', xlim=c(-5,5), ylim=c(-2,2)) # Empty plot
+
+# Cef in drinking water timeline
+rect(xleft=-4, ybottom=-0.5, xright=0, ytop=0.5, col=wes_palette("FantasticFox")[3], border='black')
+Arrows(x0=-4, y0=0, x1=4, y1=0, lwd=4, arr.type='triangle', arr.length=0.6, arr.width=0.2)
+segments(x0=c(-4,0,2,2.75), y0=c(0.5,0.5,0.5,0.5), x1=c(-4,0,2,2.75), y1=c(-0.5,-0.5,-0.5,-0.5), lwd=4)
+segments(x0=c(-4,-3,-2,-1,1), y0=c(0.25,0.25,0.25,0.25,0.25), x1=c(-4,-3,-2,-1,1), y1=c(-0.25,-0.25,-0.25,-0.25,-0.25), lwd=2)
+points(x=c(0,2,2.75), y=c(0.9,0.9,0.9), pch=25, bg=c('white','darkorchid3','black'), col='black', cex=3.4)
+text(x=c(-4,0,2,2.75), y=c(-0.8,-0.8,-0.8,-0.8), c('Day -7', 'Day -2', 'Day 0', '18 hrs'), cex=1.3)
+text(x=-4.6, y=0, 'b', cex=2, font=2)
+
+#----------------------------#
+
+par(mar=c(0,0,0,0))
+plot(0, type='n', axes=F, xlab='', ylab='', xlim=c(-5,5), ylim=c(-2,2)) # Empty plot
+
+# Clinda IP injection abx timeline
+Arrows(x0=-3, y0=0, x1=0, y1=0, lwd=4, arr.type='triangle', arr.length=0.6, arr.width=0.2)
+segments(x0=c(-3,-2,-1.25), y0=c(-0.5,-0.5,-0.5), x1=c(-3,-2,-1.25), y1=c(0.5,0.5,0.5), lwd=4)
+points(x=c(-3,-2,-1.25), y=c(1,1,1), pch=c(25,25,25), bg=c(wes_palette("FantasticFox")[5],'darkorchid3','black'), col='black', cex=3.4)
+text(x=c(-3,-2,-1.25), y=c(-0.8,-0.8,-0.8), c('Day -1', 'Day 0', '18 hrs'), cex=1.3)
+text(x=-3.6, y=0, 'c', cex=2, font=2)
 
 # Legend
-legend(x=0, y=1.1, legend=expression('Antibiotic in Drinking Water', 'IP Injection of Antibiotic', paste(italic('C. difficile'), ' Spore Gavage'), 'Euthanize & Necropsy'), 
-       pt.bg=c('darkorchid2','chocolate1','white','black'), pch=c(22,25,25,25), pt.cex=c(2.5,2,2,2), bty='n')
+legend(x=0.9, y=1.4, legend=expression('Streptomycin in drinking water', 'Cefoperazone in drinking water', 'Switch to untreated drinking water', 'Clindamycin IP injection', paste(italic('C. difficile'), ' str. 630 spore gavage'), 'Euthanize & necropsy'), 
+       pt.bg=c(wes_palette("FantasticFox")[1],wes_palette("FantasticFox")[3],'white',wes_palette("FantasticFox")[5],'darkorchid3','black'), cex=1.5,  pch=c(22,22,25,25,25,25), pt.cex=c(3.5,3.5,3,3,3,3), bty='n')
 
-dev.off()
+#----------------------------#
+
+par(mar=c(0,0,0,0))
+plot(0, type='n', axes=F, xlab='', ylab='', xlim=c(-10,10), ylim=c(-4,4)) # Empty plot
+
+# Antibiotic table
+#grid.table(abx_table, rows=NULL)
 
 #-------------------------------------------------------------------------------------------------------------------------------------#
 
 #Clean up
-rm(plot_file)
+dev.off()
+rm(plot_file, abx_table_file, abx_table)
 for (dep in deps){
   pkg <- paste('package:', dep, sep='')
   detach(pkg, character.only = TRUE)
