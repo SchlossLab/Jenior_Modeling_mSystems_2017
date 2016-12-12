a12835ec177ee678ed94e4c69dd2dfedb668b340
Matt Jenior <mljenior@umich.edu>
2016-12-12 16:34:59 -0500

    working on fig S5 and recalculating confidences

--- code/R/supplement/fig_S5.R
+++ code/R/supplement/fig_S5.R
@@ -1,35 +1,64 @@
 
-# Read in score distribution file
-scores <- read.delim('~/Desktop/Repositories/Jenior_Transcriptomics_2015/data/metabolic_models/C01094_distribution.txt', header=FALSE)[,1]
+pdf(file='~/Desktop/Repositories/Jenior_Transcriptomics_2015/results/supplement/figures/figure_S5.pdf', width=14, height=5)
+layout(matrix(c(1,2), nrow=1, ncol=2, byrow = TRUE))
 
-pdf(file='~/Desktop/Repositories/Jenior_Transcriptomics_2015/results/supplement/figures/figure_S5.pdf', width=7, height=5)
+
+# Read in score distribution file
+all_scores <- read.delim('~/Desktop/Repositories/Jenior_Transcriptomics_2015/data/metabolic_models/test_distribution.txt', header=FALSE)
+fructose <- unique(all_scores[,1])
+propanoate <- unique(all_scores[,2])
 
 # Create a curve for the sample distribution
 par(mar=c(4,4,1,1))
-score_hist <- hist(scores, breaks=50, main='', xlab='Metabolite Importance Score', ylab='Score Frequency', xlim=c(-7,7), ylim=c(0,500), axes=FALSE, border='white')
+scores <- fructose
+score_hist <- hist(scores, breaks=10, main='', xlab='Metabolite Importance Score', ylab='Score Frequency', 
+                   xlim=c(-9,9), ylim=c(0,500), las=1)
 box()
-axis(1, at=seq(-9,9,3), labels=seq(-9,9,3))
-axis(2, at=seq(0,500,100), labels=seq(0,500,100), las=1)
 
-xfit <- seq(min(scores), max(scores), length=10000)
-yfit <- dnorm(xfit, mean=mean(scores), sd=sd(scores)) * diff(score_hist$mids[1:2]) * length(scores) 
-lines(xfit, yfit, lwd=2)
-
-# Calulacte condfidence
-score_median <- quantile(scores)[3]
-lower_iqr <- quantile(scores)[2]
-upper_iqr <- quantile(scores)[4]
-lower_95 <- score_median - abs(1.7 * (lower_iqr / sqrt(length(scores))))
-upper_95 <- score_median + abs(1.7 * (upper_iqr / sqrt(length(scores))))
-lower_99 <- score_median - abs(1.95 * (lower_iqr / sqrt(length(scores))))
-upper_99 <- score_median + abs(1.95 * (upper_iqr / sqrt(length(scores))))
+# Calculate stats
+all_quantile = quantile(scores)
+score_median = all_quantile[3]
+iqr = all_quantile[4] - all_quantile[2]
+numerator = 1.25 * iqr
+denominator = 1.35 * sqrt(length(scores))
+range_factor = numerator / denominator
+range_95 = 1.6 * range_factor
+lower_95 = score_median - range_95
+upper_95 = score_median + range_95
 
 # Summary lines
 abline(v=score_median, lwd=2, col='black') # Median
-abline(v=c(lower_99,upper_99), lty=2, lwd=2, col='red') # 0.99
+abline(v=c(lower_95,upper_95), lty=2, lwd=2, col='red')
 
 # Actual score for fructose 1-phosphate
-arrows(x0=5.946, y0=250, x1=5.946, y1=175, col='blue', length=0.2, angle=20, lwd=4)
-text(x=5.946, y=270, 'Measured Score')
+arrows(x0=5.946, y0=75, x1=5.946, y1=50, col='blue', length=0.2, angle=20, lwd=4)
+mtext('a', side=2, line=2, las=2, adj=1.7, padj=-10.5, cex=1.1, font=2)
+
+
+# Create a curve for the sample distribution
+par(mar=c(4,4,1,1))
+scores <- propanoate
+score_hist <- hist(scores, breaks=10, main='', xlab='Metabolite Importance Score', ylab='Score Frequency', 
+                   xlim=c(-9,9), ylim=c(0,500), las=1)
+box()
+
+# Calculate stats
+all_quantile = quantile(scores)
+score_median = all_quantile[3]
+iqr = all_quantile[4] - all_quantile[2]
+numerator = 1.25 * iqr
+denominator = 1.35 * sqrt(length(scores))
+range_factor = numerator / denominator
+range_95 = 1.6 * range_factor
+lower_95 = score_median - range_95
+upper_95 = score_median + range_95
+
+# Summary lines
+abline(v=score_median, lwd=2, col='black') # Median
+abline(v=c(lower_95,upper_95), lty=2, lwd=2, col='red')
+
+# Actual score for propanoate
+arrows(x0=5.946, y0=75, x1=5.946, y1=50, col='blue', length=0.2, angle=20, lwd=4)
+mtext('b', side=2, line=2, las=2, adj=1.7, padj=-10.5, cex=1.1, font=2)
 
 dev.off()
