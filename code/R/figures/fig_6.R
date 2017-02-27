b1caac7a26226e63123d34c02a5de6dd8c0138c9
Matt Jenior <mljenior@umich.edu>
2017-02-26 22:05:18 -0500

    finished fig 6

--- code/R/figures/fig_6.R
+++ code/R/figures/fig_6.R
@@ -18,6 +18,7 @@ metadata <- '~/Desktop/Repositories/Jenior_Transcriptomics_2015/data/metadata.ts
 
 # Metabolomic data
 metabolome <- read.delim(metabolome, sep='\t', header=T)
+annotation <- metabolome[,1:5]
 metabolome$SUPER_PATHWAY <- NULL
 metabolome$SUB_PATHWAY <- NULL
 metabolome$PUBCHEM <- NULL
@@ -26,6 +27,14 @@ metabolome <- metabolome[match(unique(metabolome$KEGG), metabolome$KEGG),]
 rownames(metabolome) <- metabolome$KEGG
 metabolome$KEGG <- NULL
 metabolome$BIOCHEMICAL <- NULL
+annotation <- subset(annotation, KEGG != 'NA')
+annotation <- annotation[match(unique(annotation$KEGG), annotation$KEGG),]
+rownames(annotation) <- annotation$KEGG
+annotation$KEGG <- NULL
+annotation$SUB_PATHWAY <- NULL
+annotation$PUBCHEM <- NULL
+annotation$BIOCHEMICAL <- NULL
+annotation$SUPER_PATHWAY <- gsub('_',' ', annotation$SUPER_PATHWAY)
 
 # Combine with metadata
 metadata <- read.delim(metadata, sep='\t', header=T, row.names=1)
@@ -111,15 +120,8 @@ germfree <- as.data.frame(cbind(combined$germfree_score, combined$germfree_conc)
 rownames(germfree) <- rownames(combined)
 colnames(germfree) <- c('score', 'conc')
 germfree$name <- combined$Compound_name
-<<<<<<< HEAD
 germfree <- subset(germfree, germfree[,1] != 0)
-rm(combined)
-
-# Subset those metabolites with only positive values
-cef <- subset(cef, score > 0 & conc > 0)
-clinda <- subset(clinda, score > 0 & conc > 0)
-strep <- subset(strep, score > 0 & conc > 0)
-germfree <- subset(germfree, score > 0 & conc > 0)
+combined <- rbind(cef, clinda, strep, germfree)
 
 # Calculate Spearman correlation
 p.adjust(c(cor.test(cef[,1], cef[,2], method='spearman', exact=FALSE)$p.value, 
@@ -130,20 +132,34 @@ c(cor.test(cef[,1], cef[,2], method='spearman', exact=FALSE)$estimate,
   cor.test(clinda[,1], clinda[,2], method='spearman', exact=FALSE)$estimate, 
   cor.test(strep[,1], strep[,2], method='spearman', exact=FALSE)$estimate, 
   cor.test(germfree[,1], germfree[,2], method='spearman', exact=FALSE)$estimate)
-=======
-germfree <- subset(germfree, germfree[,1] != 0) # remove metabolites with 0 importance
-combined <- rbind(cef, clinda, strep, germfree)
->>>>>>> master
 
 # Identify outliers with a positive importance score
 cef_outliers <- rbind(subset(cef, conc >= (as.numeric(quantile(cef$conc)[4]) + (1.5 * IQR(cef$conc)))), 
                       subset(cef, conc <= (as.numeric(quantile(cef$conc)[2]) - (1.5 * IQR(cef$conc)))))
+cef_outliers <- subset(cef_outliers, score > 0)
 clinda_outliers <- rbind(subset(clinda, conc >= (as.numeric(quantile(clinda$conc)[4]) + (1.5 * IQR(clinda$conc)))), 
                          subset(clinda, conc <= (as.numeric(quantile(clinda$conc)[2]) - (1.5 * IQR(clinda$conc)))))
+clinda_outliers <- subset(clinda_outliers, score > 0)
 strep_outliers <- rbind(subset(strep, conc >= (as.numeric(quantile(strep$conc)[4]) + (1.5 * IQR(strep$conc)))), 
                         subset(strep, conc <= (as.numeric(quantile(strep$conc)[2]) - (1.5 * IQR(strep$conc)))))
+strep_outliers <- subset(strep_outliers, score > 0)
 germfree_outliers <- rbind(subset(germfree, conc >= (as.numeric(quantile(germfree$conc)[4]) + (1.5 * IQR(germfree$conc)))), 
                            subset(germfree, conc <= (as.numeric(quantile(germfree$conc)[2]) - (1.5 * IQR(germfree$conc)))))
+germfree_outliers <- subset(germfree_outliers, score > 0)
+germfree_outliers <- subset(germfree_outliers, conc >= 3.910228)
+combined_outliers <- rbind(subset(combined, conc >= (as.numeric(quantile(combined$conc)[4]) + (1.5 * IQR(combined$conc)))), 
+                           subset(combined, conc <= (as.numeric(quantile(combined$conc)[2]) - (1.5 * IQR(combined$conc)))))
+
+# Add pathways to combined analysis
+combined_annotation <- merge(annotation, combined_outliers, by='row.names')
+rownames(combined_annotation) <- combined_annotation$Row.names
+combined_annotation$Row.names <- NULL
+
+# not merging properly...
+
+
+
+
 
 # Calculate stats
 test <- as.data.frame(cbind(round(c(lm(conc ~ score, data=strep)$coefficients[[2]],
@@ -180,110 +196,86 @@ par(las=1, mar=c(3,4,1,1), mgp=c(1.8,0.7,0))
 plot(combined[,1], combined[,2], xlab='Importance Score', ylab=expression(paste(Delta,' Scaled Intensity')), 
      pch=19, cex=1.1, xlim=c(-10,10), ylim=c(-1,15), col='gray30')
 abline(lm(conc ~ score, data=strep), col='black', lwd=2)
+legend('topright', legend='All Treatment Groups', bty='n', cex=1.2)
 mtext('A', side=2, line=2, las=2, adj=1.2, padj=-8, cex=1.2)
-legend('topleft', legend=c(as.expression(bquote(paste('m = ',.(test$m[5])))), as.expression(bquote(paste(italic('r'),' = ',.(test$r[5])))), as.expression(bquote(paste(italic('P'),' = ',.(test$p[5]),'*')))), pt.cex=0, bty='n', cex=1.1)
-legend('topright', legend='All Treatment Groups', bty='n', cex=1.1)
+
+
+legend('topleft', legend=c(as.expression(bquote(paste(italic('rho'),' = ',.(test$r[5])))), 
+                           as.expression(bquote(paste(italic('P'),' = ',.(test$p[5]),'*')))), pt.cex=0, bty='n', cex=1.1)
+
+
+points(combined_outliers[,1], combined_outliers[,2], pch=21, bg='white', col='gray30', cex=1.7, lwd=2)
+
+# lEGEND COLORING POINTS BASED ON TYPE OF MOLECULE
+
+
+# Add dotted line for outlier cutoff and frequently appearing outliers
+
+
+
+
+
 
 # streptomycin alone
 plot(strep[,1], strep[,2], xlab='Importance Score', ylab=expression(paste(Delta,' Scaled Intensity')), 
-<<<<<<< HEAD
-     pch=19, cex=0.9, xlim=c(-1,9), ylim=c(-1,6), col=wes_palette("FantasticFox")[1])
-=======
      pch=19, xlim=c(-10,10), ylim=c(-1,9), col=wes_palette("FantasticFox")[1])
->>>>>>> master
 abline(v=0, lty=2, col='gray30')
 abline(lm(conc ~ score, data=strep), col='black', lwd=2)
 mtext('B', side=2, line=2, las=2, adj=1.2, padj=-8, cex=1.2)
-legend('topleft', legend=c(as.expression(bquote(paste('m = ',.(test$m[1])))), as.expression(bquote(paste(italic('r'),' = ',.(test$r[1])))), as.expression(bquote(paste(italic('P'),' = ',.(test$p[1]))))), pt.cex=0, bty='n', cex=1.1)
-legend('topright', legend='Streptomycin-pretreated', bty='n', cex=0.9)
-# Label outliers
-<<<<<<< HEAD
-points(strep_outliers[,1], strep_outliers[,2], pch=21, bg=wes_palette("FantasticFox")[1], cex=1.5, lwd=2)
-#text(x=c(4.5,5.2,5.8,3.5,7,4,-1.5,6.2), 
-#     y=c(1.9862,4.6330,-2.5686,-7.1469,-3.5686,-5.8,-2.3,-4.8), 
-#     strep_outliers$name, cex=0.8)
-#segments(x0=c(2.6,2.47,3.6), y0=c(-3.5686,-4.57,-4.3), 
-#         x1=c(5.2,3,4.2), y1=c(-3.5686,-5.6,-4.6))
-
-=======
+legend('topleft', legend=c(as.expression(bquote(paste(italic('rho'),' = ',.(test$r[1])))), 
+                           as.expression(bquote(paste(italic('P'),' = ',.(test$p[1]))))), pt.cex=0, bty='n', cex=1.1)
+legend('topright', legend='Streptomycin', bty='n', cex=1.1)
 points(strep_outliers[,1], strep_outliers[,2], pch=21, bg=wes_palette("FantasticFox")[1], cex=1.7, lwd=2)
-text(x=c(2.2,7,5.5,4.2,-5), 
-     y=c(1.4,7.4,2.9,3.8,2.6), 
-     strep_outliers$name, cex=0.8)
-#segments(x0=c(2.6,2.47), y0=c(-3.5686,-4.57), 
-#         x1=c(5.2,3), y1=c(-3.5686,-5.6))
->>>>>>> master
+text(x=c(8,7,5.5,5,-5.6), 
+     y=c(1.8,7.3,3,4.5,2.6), 
+     strep_outliers$name, cex=0.9)
+segments(x0=c(2.5,-0.8,2.3), y0=c(4.3,2.6,2), 
+         x1=c(1.3,1,5.6), y1=c(3.5,2.3,1.9))
 
 # cefoperazone alone
 plot(cef[,1], cef[,2], xlab='Importance Score', ylab=expression(paste(Delta,' Scaled Intensity')), 
-<<<<<<< HEAD
-     pch=19, cex=0.9, xlim=c(-1,8), ylim=c(-1,4), col=wes_palette("FantasticFox")[3]) 
-=======
      pch=19, xlim=c(-10,10), ylim=c(-1,11), col=wes_palette("FantasticFox")[3]) 
->>>>>>> master
 abline(v=0, lty=2, col='gray30')
 abline(lm(conc ~ score, data=cef), col='black', lwd=2)
 mtext('C', side=2, line=2, las=2, adj=1.2, padj=-8, cex=1.2)
-legend('topleft', legend=c(as.expression(bquote(paste('m = ',.(test$m[2])))), as.expression(bquote(paste(italic('r'),' = ',.(test$r[2])))), as.expression(bquote(paste(italic('P'),' = ',.(test$p[2]))))), pt.cex=0, bty='n', cex=1.1)
-legend('topright', legend='Cefoperazone-pretreated', bty='n', cex=0.9)
-# Label outliers
+legend('topleft', legend=c(as.expression(bquote(paste(italic('rho'),' = ',.(test$r[2])))), 
+                           as.expression(bquote(paste(italic('P'),' = ',.(test$p[2]))))), pt.cex=0, bty='n', cex=1.1)
+legend('topright', legend='Cefoperazone', bty='n', cex=1.1)
 points(cef_outliers[,1], cef_outliers[,2], pch=21, bg=wes_palette("FantasticFox")[3], cex=1.7, lwd=2)
-text(x=c(5,8,7), 
+text(x=c(5,8,7.3), 
      y=c(5,7.9,3.409411), 
-     cef_outliers$name, cex=0.8)
-segments(x0=1, y0=3.5, x1=2.5, y1=4.7)
+     cef_outliers$name, cex=0.9)
+segments(x0=1, y0=3.5, x1=2.5, y1=4.6)
 
 # clindamycin alone
 plot(clinda[,1], clinda[,2], xlab='Importance Score', ylab=expression(paste(Delta,' Scaled Intensity')), 
-<<<<<<< HEAD
-     pch=19, cex=0.9, xlim=c(-1,8), ylim=c(-1,2), col=wes_palette("FantasticFox")[5])
-=======
      pch=19, xlim=c(-10,10), ylim=c(-1,5), col=wes_palette("FantasticFox")[5])
->>>>>>> master
 abline(v=0, lty=2, col='gray30')
 abline(lm(conc ~ score, data=clinda), col='black', lwd=2)
 mtext('D', side=2, line=2, las=2, adj=1.2, padj=-8, cex=1.2)
-legend('topleft', legend=c(as.expression(bquote(paste('m = ',.(test$m[3])))), as.expression(bquote(paste(italic('r'),' = ',.(test$r[3])))), as.expression(bquote(paste(italic('P'),' = ',.(test$p[3]),'*')))), pt.cex=0, bty='n', cex=1.1)
-legend('topright', legend='Clindamycin-pretreated', bty='n', cex=0.9)
-# Label outliers
-<<<<<<< HEAD
-points(clinda_outliers[,1], clinda_outliers[,2], pch=21, bg=wes_palette("FantasticFox")[5], cex=1.5, lwd=2)
-#text(x=c(6.3,-4,1,3.9,1.585,4.4,-3.1), 
-#     y=c(1.5,0.75,1.3,-1.45,-2,-0.7,-0.7368), 
-#     clinda_outliers$name, cex=0.8)
-=======
+legend('topleft', legend=c(as.expression(bquote(paste(italic('rho'),' = ',.(test$r[3])))), 
+                           as.expression(bquote(paste(italic('P'),' = ',.(test$p[3]),'*')))), pt.cex=0, bty='n', cex=1.1)
+legend('topright', legend='Clindamycin', bty='n', cex=1.1)
 points(clinda_outliers[,1], clinda_outliers[,2], pch=21, bg=wes_palette("FantasticFox")[5], cex=1.7, lwd=2)
-text(x=c(1.723,7.06,1.222,4.170,1), 
-     y=c(2.2279556,4.3250069,1.9004303,2.8524590,0.1677397), 
-     clinda_outliers$name, cex=0.8)
->>>>>>> master
-#segments(x0=c(-0.4,3,1.6), y0=c(0.75,1.15,-1.1), 
-#         x1=c(0.7,3.9,1.6), y1=c(0.65,0.75,-1.8))
+text(x=c(4.7,7.2,-5,5.7,5.5), 
+     y=c(2.2279556,4,2.2,3.2,-0.1677397), 
+     clinda_outliers$name, cex=0.9)
+segments(x0=-0.5, y0=2.2, x1=0.6, y1=2)
 
 # germfree alone
 plot(germfree[,1], germfree[,2], xlab='Importance Score', ylab=expression(paste(Delta,' Scaled Intensity')), 
-<<<<<<< HEAD
-     pch=19, cex=0.9, xlim=c(-1,8), ylim=c(-1,15), col='forestgreen')
-=======
-     pch=19, cex=0.9, xlim=c(-10,10), ylim=c(-1,17), col='forestgreen')
->>>>>>> master
+     pch=19, cex=0.9, xlim=c(-8,8), ylim=c(-1,17), col='forestgreen', xaxt='n')
+axis(side=1, at=c(-8,-4,0,4,8), labels=c(-8,-4,0,4,8))
 abline(v=0, lty=2, col='gray30')
 abline(lm(conc ~ score, data=germfree), col='black', lwd=2)
 mtext('E', side=2, line=2, las=2, adj=1.2, padj=-8, cex=1.2)
-legend('topleft', legend=c(as.expression(bquote(paste('m = ',.(test$m[4])))), as.expression(bquote(paste(italic('r'),' = ',.(test$r[4])))), as.expression(bquote(paste(italic('P'),' = ',.(test$p[4]),'*')))), pt.cex=0, bty='n', cex=1.1)
-legend('topright', legend='Gnotobiotic', bty='n', cex=0.9)
-bquote(paste(italic('p'),' = ', .(strep_final_p[i2]), .(strep_sig[i2]), sep=''))
-# Label outliers
+legend('topleft', legend=c(as.expression(bquote(paste(italic('rho'),' = ',.(test$r[4])))), 
+                           as.expression(bquote(paste(italic('P'),' = ',.(test$p[4]),'*')))), pt.cex=0, bty='n', cex=1.1)
+legend('topright', legend='Gnotobiotic', bty='n', cex=1.1)
 points(germfree_outliers[,1], germfree_outliers[,2], pch=21, bg='forestgreen', cex=1.5, lwd=2) # color outliers
-<<<<<<< HEAD
-#text(x=c(1.950,4.2,3.358,3,3.4), 
-#     y=c(4,3.2,6.7,9.7564,12.0140), 
-#     germfree_outliers$name, cex=0.8)
-=======
-text(x=c(3,2,-2,6,6,6,7,4.087), 
-     y=c(5.4,6.7,3.197584,14.439394,6,11.890565,2,3.419291), 
-     germfree_outliers$name, cex=0.8)
->>>>>>> master
+text(x=c(4,3.2,5.7,5.9), 
+     y=c(4.523075,5.924171,14.439394,11.890565), 
+     germfree_outliers$name, cex=0.9)
 
 dev.off()
 
